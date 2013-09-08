fe8fcce13a8e79f02024e649ac31735e4a1d0c06
Nathan van Geffen <nathan.van.geffen@hva.nl>
2013-09-08 12:52:19 +0200

    Functie giveInt weggehaald

--- Demo_AndroidAppProject/Demo_AndroidApp/src/main/java/com/example/demo_androidapp/MainActivity.java
+++ Demo_AndroidAppProject/Demo_AndroidApp/src/main/java/com/example/demo_androidapp/MainActivity.java
@@ -18,10 +18,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
-    public int giveInt()
-    {
-        return 1;
-    }
     
 }
