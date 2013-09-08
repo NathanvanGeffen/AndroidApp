2e21bffc9da57242df143441473e2ce2eadbe643
Nathan van Geffen <nathan.van.geffen@hva.nl>
2013-09-08 13:26:35 +0200

    ShowMessage Button added

--- Demo_AndroidAppProject/Demo_AndroidApp/src/main/java/com/example/demo_androidapp/MainActivity.java
+++ Demo_AndroidAppProject/Demo_AndroidApp/src/main/java/com/example/demo_androidapp/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.demo_androidapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    Button showMessagebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showMessagebtn = (Button) findViewById(R.id.shwMsg);
+
+        showMessagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Yes", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
